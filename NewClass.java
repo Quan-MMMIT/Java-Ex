c4d186999ab52ad6342364db5bf3086d9e31e669
Quan-MMMIT <127724092+Quan-MMMIT@users.noreply.github.com>
2023-03-13 14:31:55 +0700

    Add files via upload
    datatype Java

---
+++ NewClass.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LE MINH QUAN
+ */
+public class NewClass {
+    
+}
+public class xuatnhap {
+    public static void main(String{) args){
+        boolean flag = true;
+        int a = 15;
+        byte b = 2;
+        double c = 12.6;
+        float d = 4.5;
+        String a = "hello world";
+        char ch = 'A';
+        
+        Scanner ac = new Scanner(System.in);
+        System.out.print("Hay nhap ho ten: ");
+        String hoten = sc.nextline();
+        System.out.println("Ban da nhap ho ten la:");
+    }
+}
