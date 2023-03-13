7b2127eb4bc03c80d058f5ad30960e0ce6195eec
Quan-MMMIT <127724092+Quan-MMMIT@users.noreply.github.com>
2023-03-13 15:21:54 +0700

    Add files via upload

---
+++ IFELSE2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LE MINH QUAN
+ */
+public class IFELSE2 {
+    public static void main(String[] args) {
+        int number = 13;
+        if (number % 2 == 0) {
+            System.out.println("Số " + number + " là số chẵn.");
+        } else {
+            System.out.println("Số " + number + " là số lẻ.");
+        }
+    }
+}
