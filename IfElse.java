8f73a65bc397cf200b8d8a068107187af9a44a15
Quan-MMMIT <127724092+Quan-MMMIT@users.noreply.github.com>
2023-03-13 15:20:41 +0700

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LE MINH QUAN
+ */
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age > 18) {
+            System.out.print("Tuổi lớn hơn 18");
+        }
+    }
+}
