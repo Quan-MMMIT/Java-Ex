5d189e9f50d8b823f9652dc36f16182f647de5d2
Quan-MMMIT <127724092+Quan-MMMIT@users.noreply.github.com>
2023-03-13 15:27:51 +0700

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LE MINH QUAN
+ */
+public class Switch {
+    public static void main(String[] args) {
+        int number = 20;
+        switch (number) {
+        case 10:
+            System.out.println("10");
+            break;
+        case 20:
+            System.out.println("20");
+            break;
+        case 30:
+            System.out.println("30");
+            break;
+        default:
+            System.out.println("Not in 10, 20 or 30");
+        }
+    }
+}
