8617cf0c0f89a41512f9b806a82db3c90d7e0295
Quan-MMMIT <127724092+Quan-MMMIT@users.noreply.github.com>
2023-03-14 09:27:44 +0700

    Add files via upload

---
+++ FornWhile.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LE MINH QUAN
+ */
+public class ForExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+public class WhileExample1 {
+ public static void main(String[] args) {
+  int i = 1;
+  while (i <= 10) {
+   System.out.println(i);
+   i++;
+  }
+ }
+}
